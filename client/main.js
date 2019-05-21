233eb2e09a77367079561b2ae6d923f2f9a05c0c
charles <charles@charlesui-MacBookPro.local>
2019-05-21 19:06:51 +0900

    First commit

---
+++ client/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import VueMeteorTracker from 'vue-meteor-tracker';
+Vue.use(VueMeteorTracker);
+
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+
+import App from './App.vue';
+import './main.html';
+
+
+Meteor.startup(() => {
+  new Vue({
+    el: '#app',
+    ...App,
+  });
+});
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
