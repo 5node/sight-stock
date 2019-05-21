fa43f5075328eac5e825dd93b555ef80cf842ec7
이준혁 <leejh10003@gmail.com>
2019-05-22 00:57:20 +0900

    Revert into vue-router remove

--- client/components/ConetentCard.vue
+++ client/components/ConetentCard.vue
@@ -3,17 +3,11 @@
 		<h1>제목: {{content.title}}</h1>
 		<p>제작자: {{content.creater}}</p>
 		<p>미리보기</p>
-		<img :src=content.imageAddress>
+		<img :src="content.imageAddress">
 	</div>
 </template>
 <script>
-import router from '../router/index';
 export default {
-	props: ['content'],
-	methods:{
-		navigate(){
-			router.go(-1)
-		}
-	}
-}
+	props: ["content"]
+};
 </script>
